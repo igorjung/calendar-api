3060c3313212b20e82ae1ac35d45d5cd5214e95a
igorjung <igor.jung@outlook.com>
2021-08-31 22:10:02 -0300

    Adicionando login e register

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
-import { Router } from 'express';
+// Controllers
+import AuthController from './app/controllers/AuthController';
+
+// Middlewares
+// import token from './app/middlewares/token';
+
+// Dependencies
+const { Router } = require('express');
 
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Welcome to Calendar API' }));
+
+// Auth
+routes.post('/auth/register', AuthController.register);
+routes.post('/auth/login', AuthController.login);
 
 export default routes;
